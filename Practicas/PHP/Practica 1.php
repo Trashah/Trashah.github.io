5f026c17d4fd9216fefea405ba0018b635f48fe5
Trashah <l21530323@cancun.tecnm.mx>
2024-11-07 00:19:38 -0500

    Updates on relative paths of index.html

---
+++ Practicas/PHP/Practica 1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang = "es">
+
+    <head>
+
+        <title> Primer PHP </title>
+
+    </head>
+
+    <body>
+
+        <?php
+
+        echo "<h3> ¡Hola! ¡Este es como mi quinto script en PHP! </h3>";
+
+        // Comentario
+        # Otro comentario
+
+        /* Esto es un bloque de mas de una línea
+        de comentario */
+
+        $texto = "<p> ¡¡Este texto esta guardado en una variable de PHP!! </p>";
+        echo $texto;
+
+        $numero1 = 5;
+        $numero2 = 6;
+        $resultado = $numero1 + $numero2;
+        echo "<p> ¡¡¡El resultado de la sum de $numero1 y $numero2 es $resultado!!! </p>";
+
+        ?>
+
+    </body>
+
+</html>
