0e43bc8379c35d1c8270700b9237aede6fc54c29
Traxus Laptop <l21530323@cancun.tecnm.mx>
2024-11-10 16:30:50 -0500

    Upload Practica 2.php

---
+++ Practicas/PHP/Practica 2.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<?php
+
+$mensajeGlobal = "Holaaaa esto es un mensaje global";
+$numeroGlobal = 8;
+
+function imprimirMensajeLocal() {
+    $mensajeLocal = "Holaaaaa como estas te estoy saludando desde un mensaje local";
+    echo $mensajeLocal;
+}
+
+function imprimirMensajeGlobal() {
+    echo $mensajeGlobal;
+}
+
+function imprimirNumeroGlobalYLocal1() {
+    $numeroLocal = 3;
+    echo "Numero Global = $numeroGlobal";
+    echo "<br>";
+    echo "Numero Local = $numeroLocal";
+}
+
+function imprimirNumeroGlobalYLocal2() {
+    echo "Numero Local = $numeroLocal";
+    echo "<br>";
+    echo "Numero Global = $numeroGlobal";
+}
+
+?>
+
+<html>
+
+    <body>
+
+        <?php imprimirMensajeLocal(); ?>
+        <br>
+        <?php imprimirNumeroGlobalYLocal1(); ?>
+        <br><br>
+
+        <?php 
+        echo "Numero Local = $numeroLocal";
+        echo "<br>";
+        echo "Numero Global = $numeroGlobal";
+        ?>
+
+    </body>
+
+</html>
+
+<!-- 
+
+NOTAS
+
+== ==== <> != !== > < >= <=
+= == -= *= /= %=
