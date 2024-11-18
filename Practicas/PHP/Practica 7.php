f248aa4630f703fcbccc54f9cf8a4e1f3b53ecd4
Trashah <l21530323@cancun.tecnm.mx>
2024-11-18 13:28:52 -0500

    Update Practica 7

--- Practicas/PHP/Practica 7.php
+++ Practicas/PHP/Practica 7.php
@@ -20,18 +20,18 @@
 
 			$conexion = mysqli_connect($servidor,$usuario,$password,$baseDeDatos);
 
-			$nombreNuevaBaseDeDatos = "Pogramacion_Web_Uriel_php_Nuevo";
+			$nombreNuevaBaseDeDatos = "Programacion_Web_Uriel_php_Nuevo";
 
 			$consulta = "CREATE DATABASE $nombreNuevaBaseDeDatos";
 
-			if ($resultado = mysqli_query($conexion,$consulta)){
+			if ($resultado = mysqli_query($conexion,$consulta)) {
 				echo "La base de datos $nombreNuevaBaseDeDatos, ha sido creata de forma exitosa";
 				echo "<br>";
 				echo "El script utilizado fue: $consulta";
 			}
 
-			else{
-				"La base de datos $nombreNuevaBaseDeDatos no se ha podido crear<br>".mysqli_error($conexion);
+			else {
+				"La base de datos $nombreNuevaBaseDeDatos no se ha podido crear" . mysqli_error($conexion);
 			}
 
 			mysqli_close($conexion);
